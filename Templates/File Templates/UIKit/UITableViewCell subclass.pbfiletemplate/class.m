8c8841b2e26d0dd7558b1754172b561ced501c01
Jesse Armand <mnemonic.fx@gmail.com>
2010-07-13 03:33:09 +0700

    Added UIKit templates

---
+++ Templates/File Templates/UIKit/UITableViewCell subclass.pbfiletemplate/class.m
@@ -0,0 +1,41 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+- (void)layoutSubviews {
+    [super layoutSubviews];
+	// Lays out subviews
+}
+*/
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
---
+++ Templates/File Templates/UIKit/UIView subclass.pbfiletemplate/class.m
@@ -0,0 +1,42 @@
+//
+//  «FILENAME»
+//  «PROJECTNAME»
+//
+//  Created by «FULLUSERNAME» on «DATE».
+//  Copyright «YEAR» «ORGANIZATIONNAME». All rights reserved.
+//
+
+«OPTIONALHEADERIMPORTLINE»
+
+@implementation «FILEBASENAMEASIDENTIFIER»
+
+
+- (id)initWithFrame:(CGRect)frame {
+	self = [super initWithFrame:frame];	
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+/*
+- (void)layoutSubviews {
+    [super layoutSubviews];
+	// Lays out subviews
+}
+*/
+
+- (void)dealloc {
+    [super dealloc];
+}
+
+
+@end
